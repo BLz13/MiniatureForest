27eabaa7b28e394ad268f20821cfc4984826ca02
Blz13 <bruno.lazo@outlook.com>
2023-01-19 18:24:13 -0300

    Lazo+EntregaFinal1.6

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -16,29 +16,45 @@ function NavBar() {
     const [sidebarStatus, setSidebar] = useState(true);
 
     const [navbarStatus, setNavbar] = useState(true);
-
-    const [menuIconStatus, setMenuIcon] = useState(true);
     
-    const menuIconHandler = () => {
+    const menuIconHandlerNavOff = () => {
         const filler = document.getElementById("Filler");
         const lines = document.getElementById("Lines");
         const menuIcon = document.getElementById("Menu");
         let fillerClass;
         let linesClass;
         let iconClass;
-        if (menuIconStatus===true) {
-            fillerClass = sidebarStatus ? "hide" : "close";
-            linesClass = sidebarStatus ? "open" : "hide";
-            iconClass="menuIcon"
-        } {
-            fillerClass = sidebarStatus ? "hide" : "closeNavON";
-            linesClass = sidebarStatus ? "open" : "hide";
-            iconClass="menuIconNavON"
-        }        
+        linesClass = sidebarStatus ? "hide" : "closeNavON";
+        fillerClass = sidebarStatus ? "open" : "hide";
+        iconClass="menuIconNavON"
         filler.setAttribute("class", fillerClass);
         lines.setAttribute("class", linesClass);
         menuIcon.setAttribute("class", iconClass);
     }
+
+    const menuIconHandlerNavOn = () => {
+        const filler = document.getElementById("Filler");
+        const lines = document.getElementById("Lines");
+        const menuIcon = document.getElementById("Menu");
+        let fillerClass;
+        let linesClass;
+        let iconClass;
+        fillerClass = !sidebarStatus ? "hide" : "close";
+        linesClass = !sidebarStatus ? "open" : "hide";
+        iconClass="menuIcon"
+        filler.setAttribute("class", fillerClass);
+        lines.setAttribute("class", linesClass);
+        menuIcon.setAttribute("class", iconClass);
+    }
+
+    const menuClickHandler = () => {
+        setSidebar(!sidebarStatus);
+        (window.scrollY >= 50) ? (
+            setNavbar(false)
+        ) : (
+            setNavbar(!navbarStatus)
+        )
+    };
 
     const onScrollHandler = () => {
         (window.scrollY >= 50) ? (
@@ -50,27 +66,6 @@ function NavBar() {
 
     window.addEventListener("scroll", onScrollHandler);
     
-    const menuClickHandler = () => {
-        setSidebar(!sidebarStatus);
-        (window.scrollY >= 50) ? (
-            setNavbar(false)
-        ) : (
-            setNavbar(!navbarStatus)
-        )
-    };
-    
-    useEffect(() => {
-        menuIconHandler();
-}, [menuIconStatus]);
-    
-    useEffect(() => {
-        if (navbarStatus===true) {
-            setMenuIcon(true);
-        } {
-            setMenuIcon(false);
-        }
-}, [navbarStatus]);
-
     useEffect(() => {
         document.onmousedown = (event) =>{
             if ( (sidebarStatus === false) && (!sidebarRef.current.contains(event.target)) && (!menuIconRef.current.contains(event.target)) ){
@@ -79,6 +74,10 @@ function NavBar() {
             }
         }
     })
+
+    useEffect( () => {
+        navbarStatus ? menuIconHandlerNavOn() : menuIconHandlerNavOff(); 
+    },[navbarStatus,sidebarStatus])
 
     return(
         <nav className={navbarStatus ? "navBar transOn" : "navBar transOff"}>
